6d1aa4d110f536732fec977497e61c023894f0d0
Dejan <dejan@codingharbour.com>
2021-04-27 23:42:24 +0200

    cleanup

--- kafka-ssl/security/remove-certs.sh
+++ kafka-ssl/security/remove-certs.sh
@@ -4,11 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cd $DIR && rm root.crt root.key consumer.properties producer.properties credentials truststore.jks
 
-# for i in broker
-# do
-#     rm $i-signed.crt $i.csr $i.keystore.jks
-# done
-
 for i in broker zookeeper producer consumer
 do
     rm $i-signed.crt $i.key $i.csr $i-keypair.pem
